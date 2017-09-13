a07c211256ca957d11517f60041b625d7fc8a2f8
Joel Romero <joelrg1288@gmail.com>
2017-09-13 09:46:34 -0500

    Correciones menores.

---
+++ Prácticas/Bajar Volumen/Cabecera.h
@@ -0,0 +1,25 @@
+typedef struct CABECERA
+{
+	char ChunkID[4];					//Contiene las 'RIFF'
+	unsigned int ChunkSize;				//Contiene el tamaño total sin contar este y el segmento anterior (8 bytes)
+	unsigned char Format[4];			//Contiene 'WAVE'
+
+	//Aqui comienza el primer subchunk 'fmt'
+	char SubChunk1ID[4];				//Contiene 'fmt'
+	unsigned int SubChunk1Size;			//Contiene el tamaño del resto de el primer subchunk
+	unsigned int AudioFormat;			//Formato de audio, es es distinto de 1, es forma de compresión
+	unsigned int NumChannels;			//Numero de canales, mono = 1, estereo = 2, etc.
+	unsigned int SampleRate;			//8000, 44100, etc.
+	unsigned int ByteRate;				//(SampleRate * Numero canales * Bits per Sample) / 8
+	unsigned int BlockAlign;			//(Numero canales * Bits per Sample) / 8
+	unsigned int BitsPerSample;			//8 bits, 16 bits, etc.
+	
+	//Aqui comienza el segundo subchunk 'data'
+	char SubChunk2ID[4];				//Contiene 'data'
+	unsigned int SubChunk2Size;			//Numero de bytes en los datos, es decir, bytes despues de este segmento
+	unsigned int data;					//Datos de sonidos reales
+	
+}cabecera;
+
+FILE * abreArchivo (char * nombreArch, char * nombreModificado, int tipo);				//Para abrir los archivos de entrada y salida
+void leerCabecera (FILE * archivoEntrada, FILE * archivoSalida, cabecera * cab);		//Para copiar e imprimir la cabecera
---
+++ Prácticas/Bajar Volumen/Volumen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "Cabecera.c"
+
+int main(int argc, char const *argv[])
+{
+	FILE * archivoEntrada, * archivoSalida;
+	cabecera cab;
+	int i, lectura, escritura;
+	short muestra;
+	char * nombreModificado = (char *) malloc (sizeof (char));
+	char * nombreArch = (char *) malloc (sizeof (char));
+	system ("cls");
+	if (argc < 3)
+		printf("Error, faltan argumentos.\n");
+	else
+	{
+		nombreArch = (char *) argv [1];
+		nombreModificado = (char *) argv [2];
+	}
+
+	//Abrimos los archivos en modo binario
+	archivoEntrada = abreArchivo (nombreArch, nombreModificado,1);
+	archivoSalida = abreArchivo (nombreArch, nombreModificado, 2);
+
+	//Leemos e imprimimos la cabecera del archivo wav
+	leerCabecera (archivoEntrada, archivoSalida, &cab);
+	
+	for (i = 0; i < (cab.SubChunk2Size); i ++)
+	{
+		lectura = fread(&muestra, sizeof (short), 1, archivoEntrada);
+		muestra *= 0.5;																		//Dividimos a la mitad cada uno de los datos
+		escritura = fwrite(&muestra, sizeof (short), lectura, archivoSalida);				//Escribimos los datos nuevos en el archivo
+	}
+	printf ("\n\n");
+	fclose (archivoEntrada);
+	fclose (archivoSalida);
+	printf ("Archivo '%s' modificado correctamente.\n", nombreArch);
+	return 0;
+}
