68b84a7ea223d435105fde94fe7058641d10d432
Joel Romero <joelrg1288@gmail.com>
2017-10-12 02:42:11 -0500

    Actualización de código, falta completar reporte

---
+++ Prácticas/2 - Convolución/Cabecera.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Cabecera.h"
+#define PI 3.14159265
+#define FREC_CORTE 1000
+#define TAM_ARREGLO 20
+
+int i;					//Variable global para manejar ciclos
+
+FILE * abreArchivo (char * nombreArch, char * nombreModificado, int tipo)
+{
+	FILE * pt1, * pt2;
+	pt1 = fopen (nombreArch,"rb");
+	if (pt1 == NULL)
+	{
+		printf("Error al abrir archivo '%s'.\n", nombreArch);
+		exit(0);
+	}
+	
+	//Abrimos el archivo a escribir en modo binario	
+	pt2 = fopen (nombreModificado,"wb");
+	if (pt2 == NULL)
+	{
+		printf("Error al crear el archivo '%s'.\n", nombreModificado);
+		exit(1);
+	}
+	if (tipo == 1)
+	{
+		printf("Archivo '%s' abierto correctamente.\n", nombreArch);
+		return pt1;
+	}
+	else
+	{
+		printf("Archivo '%s' creado correctamente.\n", nombreModificado);
+		return pt2;
+	}
+}
+
+void leerCabecera (FILE * archivoEntrada, FILE * archivoSalida, cabecera * cab)
+{
+	unsigned char buffer4[4], buffer2 [2];
+
+	//ChunkID 
+	fread(cab -> ChunkID,sizeof(cab -> ChunkID),1,archivoEntrada);
+	fwrite (cab -> ChunkID, sizeof (cab -> ChunkID), 1, archivoSalida);
+
+	//ChunkSize 
+	fread(buffer4, sizeof(buffer4),1,archivoEntrada);
+	fwrite(buffer4, sizeof(buffer4),1,archivoSalida);
+	cab -> ChunkSize = (buffer4[0] | (buffer4[1]<<8) | (buffer4[2]<<16) | (buffer4[3]<<24));
+
+	//Formato "Fmt"
+	fread(cab -> Format, sizeof(cab -> Format),1,archivoEntrada);
+	fwrite (cab -> Format, sizeof (cab -> Format), 1, archivoSalida);
+
+	//SubChunk1ID Formato de datos "fmt"
+	fread(cab -> SubChunk1ID,sizeof(cab -> SubChunk1ID),1,archivoEntrada);
+	fwrite (cab -> SubChunk1ID, sizeof (cab -> SubChunk1ID), 1, archivoSalida);
+
+	//SubChunk1Size
+	fread(buffer4, sizeof(buffer4),1,archivoEntrada);
+	fwrite(buffer4, sizeof(buffer4),1,archivoSalida);
+	cab -> SubChunk1Size = buffer4[0] | (buffer4[1]<<8) | (buffer4[1]<<16) | (buffer4[1]<<24);	
+	
+	// Formato de audio
+	fread(buffer2, sizeof(buffer2),1,archivoEntrada);
+	fwrite(buffer2, sizeof(buffer2),1,archivoSalida);
+	cab -> AudioFormat = buffer2[0] | (buffer2[1]<<8);
+	
+	//Canales
+	fread(buffer2, sizeof(buffer2),1,archivoEntrada);
+	fwrite(buffer2, sizeof(buffer2),1,archivoSalida);
+	cab -> NumChannels = buffer2[0] | (buffer2[1]<<8);
+
+	//SampleRate
+	fread(buffer4, sizeof(buffer4),1,archivoEntrada);
+	fwrite(buffer4, sizeof(buffer4),1,archivoSalida);
+	cab -> SampleRate = buffer4[0] | (buffer4[1]<<8) | (buffer4[2]<<16) | (buffer4[3]<<24);	
+	
+	//ByteRate
+	fread(buffer4, sizeof(buffer4),1,archivoEntrada);
+	fwrite(buffer4, sizeof(buffer4),1,archivoSalida);
+	cab -> ByteRate = buffer4[0] | (buffer4[1]<<8) | (buffer4[2]<<16) | (buffer4[3]<<24);	
+
+	//Block Align
+	fread(buffer2, sizeof(buffer2),1,archivoEntrada);
+	fwrite(buffer2, sizeof(buffer2),1,archivoSalida);
+	cab -> BlockAlign = buffer2[0] | (buffer2[1]<<8);
+	
+	//Bits per Sample
+	fread(buffer2, sizeof(buffer2),1,archivoEntrada);
+	fwrite(buffer2, sizeof(buffer2),1,archivoSalida);
+	cab -> BitsPerSample = buffer2[0] | (buffer2[1]<<8);
+
+	//SubChunk2ID
+	fread(cab -> SubChunk2ID,sizeof(cab -> SubChunk2ID),1,archivoEntrada);	
+	fwrite(cab -> SubChunk2ID,sizeof(cab -> SubChunk2ID),1,archivoSalida);
+
+	//SubChunk2Size
+	fread(buffer4, sizeof(buffer4),1,archivoEntrada);
+	fwrite(buffer4, sizeof(buffer4),1,archivoSalida);
+	cab -> SubChunk2Size = buffer4[0] | (buffer4[1]<<8) | (buffer4[2]<<16) | (buffer4[3]<<24);
+}
+
+void imprimir_cabecera (cabecera * cab)
+{
+	char * formatoArchivo = (char *) malloc (sizeof (char));
+	printf("\n\n\n(1-4) Chunk ID: %s\n",cab -> ChunkID);
+	printf("(5-8) ChunkSize:  %u\n",cab -> ChunkSize);
+	printf("(9-12) Format: %s\n",cab -> Format);
+	printf("(13-16) SubChunk 1 ID: %s\n",cab -> SubChunk1ID);
+	printf("(17-20) SubChunk 1 Size: %u\n",cab -> SubChunk1Size);
+	if (cab -> AudioFormat == 1)
+			strcpy(formatoArchivo,"PCM");
+		printf("(21-22) Audio Format: %u,%s\n",cab -> AudioFormat,formatoArchivo);
+	if (cab -> NumChannels == 1)
+			strcpy(formatoArchivo,"Mono");
+		else 
+			strcpy(formatoArchivo,"Stereo");
+		printf("(23-24) Number of Channels: %u, Tipo: %s\n",cab -> NumChannels,formatoArchivo);
+	printf("(25-28) Sample Rate: %u\n",cab -> SampleRate);
+	printf("(29-32) Byte Rate: %u BitRate: %u\n",cab -> ByteRate,cab -> ByteRate*8);
+	printf("(33-34) Block Align: %u\n",cab -> BlockAlign);
+	printf("(35-36) Bits Per Sample: %u\n",cab -> BitsPerSample);
+	printf("(37-40) SubChunk 2 ID: %s\n",cab -> SubChunk2ID);
+	printf("(41-44) SubChunk 2 Size: %u\n",cab -> SubChunk2Size);
+}
+
+float * generaImpulso ()
+{
+	float * impulso = (float *) malloc (sizeof (float) * TAM_ARREGLO);
+	for (i = 0; i < TAM_ARREGLO; i ++)
+	{
+		impulso [i] = (exp ((-2 * PI * FREC_CORTE * i) / 44100));			//Fórmula de filtro RC con fc = 1,000 Hz
+	}
+	return impulso;
+}
+
+float convolucion (float * entrada, float * impulso)
+{
+	float respuesta = 0;
+
+	//Realizamos la convolución
+	for (i = 0; i < TAM_ARREGLO; i ++)
+		if (entrada [i] != 0)
+			respuesta += (entrada [i] * impulso [i]);
+
+	//Como entran valores entre -1 y 1, comprobamos la respuesta, para saber que enviamos (aun así todos los multiplicamos por 32767 que es
+	//el valor máximo que puede tomar un short)
+	if (respuesta > 1)
+		return (1 * 32767);
+	if (respuesta < -1)
+		return (-1 * 32767);
+	else
+		return (respuesta * 32767);
+}
---
+++ Prácticas/2 - Convolución/Convolucion.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "Cabecera.c"
+#define TAM_ARREGLO 20
+
+int main(int argc, char const *argv[])
+{
+	FILE * archivoEntrada, * archivoSalida;
+	cabecera cab;
+	int i, lectura, escritura, j = 0, k;
+	short muestra;
+	float * impulso = (float *) malloc (sizeof (float) * 20);
+	float * entrada = (float *) malloc (sizeof (float) * 20);
+	char * nombreModificado = (char *) malloc (sizeof (char));
+	char * nombreArch = (char *) malloc (sizeof (char));
+	system ("cls");
+	if (argc < 3)
+	{
+		printf("Error, faltan argumentos.\n");
+		printf ("Ejemplo: '%s Archivo1.wav Salida.wav'\n\n", argv [0]);
+		exit (0);
+	}
+	else
+	{
+		nombreArch = (char *) argv [1];
+		nombreModificado = (char *) argv [2];
+	}
+
+	//Abrimos los archivos en modo binario
+	archivoEntrada = abreArchivo (nombreArch, nombreModificado,1);
+	archivoSalida = abreArchivo (nombreArch, nombreModificado, 2);
+
+	//Leemos e imprimimos la cabecera del archivo wav
+	leerCabecera (archivoEntrada, archivoSalida, &cab);
+	imprimir_cabecera (&cab);
+
+	//Generamos la respuesta al impulso
+	impulso = generaImpulso ();
+	float max = 0;
+	for (i = 0; i < TAM_ARREGLO; i ++)
+		max += (impulso [i] * 32767);
+
+	//Llenamos el arreglo de entrada con puros ceros
+	memset (entrada, 0, TAM_ARREGLO);
+	
+	//Escribimos el resto de los datos realizando la convolución
+	for (i = 0; i < cab.SubChunk2Size; i ++, j++)
+	{
+		lectura = fread(&muestra, sizeof (short), 1, archivoEntrada);
+		for (k = (TAM_ARREGLO - 1); k >= 0; k --)
+			entrada [k] = entrada [k - 1];
+		entrada [0] = (muestra / max);																//Insertamos los datos en el arreglo
+		muestra = convolucion (entrada, impulso);
+		escritura = fwrite(&muestra, sizeof (short), lectura, archivoSalida);				//Escribimos los datos nuevos en el archivo
+	}
+	printf ("\n\n");
+	fclose (archivoEntrada);
+	fclose (archivoSalida);
+	printf ("Archivo '%s' filtrado correctamente.\n", nombreArch);
+	return 0;
+}
