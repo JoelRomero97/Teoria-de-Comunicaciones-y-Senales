65ebc7dce227ce33acda3bd9debe681ecd2b49e4
Joel Romero <joelrg1288@gmail.com>
2017-10-29 12:11:46 -0600

    Se comienza la Transformada Discreta de Fourier

--- Prácticas/4 - TDF/TDF.c
+++ Prácticas/4 - TDF/TDF.c
@@ -10,12 +10,6 @@
 //// Implementación de la Transformada Discreta de Fourier utilizando la fórmula, 	////
 //// siendo un algoritmo extremadamente costoso computacionalmente, para observar	////
 //// alguna señal en el dominio de la frecuencia, su magnitud y su fase.			////
-//// La opción 1 de la TDF mostrará en el canal izquierdo la señal original y en el ////
-//// derecho, la magnitud de la TDF.												////
-//// En la opción 2, mostrará en el canal izquierdo la parte real de la TDF y en el ////
-//// derecho, la parte imaginaria de la TDF.										////
-//// En la opción 3, mostrará en el canal izquierdo la magnitud de la TDF y en el 	////
-//// derecho, la fase de la TDF.													////
 ////																				////
 ////																				////
 //// Autor: Romero Gamarra Joel Mauricio											////
