d9edc4b3f01b3eb3305ed12cf5ca7667e673a351
Joel Romero <joelrg1288@gmail.com>
2017-12-14 22:26:55 -0600

    Práctica completa :D

--- Prácticas/3 - Multiplicación/Cabecera.h
+++ Prácticas/3 - Multiplicación/Cabecera.h
@@ -1,25 +1,22 @@
 typedef struct CABECERA
 {
-	char ChunkID[4];					//Contiene las 'RIFF'
-	int ChunkSize;						//Contiene el tamaño total sin contar este y el segmento anterior (8 bytes)
-	char Format[4];						//Contiene 'WAVE'
+	char ChunkID[4];		//Contiene 'RIFF'
+	int ChunkSize;			//Contiene el tamaño total del archivo - 8 bytes
+	char Format[4];			//Contiene 'WAVE'
 
 	//Aqui comienza el primer subchunk 'fmt'
-	char SubChunk1ID[4];				//Contiene 'fmt'
-	int SubChunk1Size;					//Contiene el tamaño del resto de el primer subchunk
-	short AudioFormat;					//Formato de audio, es es distinto de 1, es forma de compresión
-	short NumChannels;					//Numero de canales, mono = 1, estereo = 2, etc.
-	int SampleRate;						//8000, 44100, etc.
-	int ByteRate;						//(SampleRate * Numero canales * Bits per Sample) / 8
-	short BlockAlign;					//(Numero canales * Bits per Sample) / 8
-	short BitsPerSample;				//8 bits, 16 bits, etc.
+	char SubChunk1ID[4];	//Contiene 'fmt'
+	int SubChunk1Size;		//Contiene el tamaño del primer subchunk
+	short AudioFormat;		//Formato de audio
+	short NumChannels;		//Numero de canales
+	int SampleRate;			//Frecuencia de muestreo
+	int ByteRate;			//(SampleRate * Numero canales * Bits per Sample) / 8
+	short BlockAlign;		//Bytes por muestra
+	short BitsPerSample;	//8 bits, 16 bits, etc.
 	
 	//Aqui comienza el segundo subchunk 'data'
-	char SubChunk2ID[4];				//Contiene 'data'
-	int SubChunk2Size;					//Numero de bytes en los datos, es decir, bytes despues de este segmento
+	char SubChunk2ID[4];	//Contiene 'data'
+	int SubChunk2Size;		//Numero de bytes en los datos
 }cabecera;
 
-FILE * abre_archivo (char * archivo1, char * archivo2, char * salida, int tipo);
-void leer_cabecera (FILE * archivo1, FILE * archivo2, cabecera * cab1, cabecera * cab2);
 void imprimir_cabecera (cabecera * cab, int tipo);
-void copiar_cabecera (FILE * entrada, FILE * salida, cabecera * cab);
--- Prácticas/3 - Multiplicación/Multiplicacion.c
+++ Prácticas/3 - Multiplicación/Multiplicacion.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "Cabecera.c"
+#include "Cabecera.h"
+
+////////////////////////////////////////////////////////////////////////////////////////
+////								Multiplicacion.c 								////
+////																				////
+////																				////
+//// Implementacion de la multiplicacion de 2 señales en archivos wav, ya sea mono	////
+//// por mono, o estéreo por estéreo.												////
+//// Compilación: gcc Cabecera.c -o Cabecera.o -c 									////
+////			  gcc Multiplicacion.c -o Multiplicacion Cabecera.o					////
+//// Ejecuación: Multiplicacion.exe Entrada.wav Salida.wav							////
+////																				////
+////																				////
+//// Autor: Romero Gamarra Joel Mauricio											////
+////////////////////////////////////////////////////////////////////////////////////////
 
 int main (int argc, char const *argv[])
 {
-	FILE * archivo_1, *archivo_2, * archivoSalida;
+	FILE * archivo_1, *archivo_2, * salida, * grande, * chico;
 	cabecera cab_1, cab_2;
-	int i;
+	int i, numero_muestras_1, numero_muestras_2, numero_muestras, aux;
 	short real1, imaginario1, real2, imaginario2;
 	float max = 32767, real1_1, imaginario1_1, real2_1, imaginario2_1;
-	char * salida = (char *) malloc (sizeof (char));
 	char * archivo1 = (char *) malloc (sizeof (char));
 	char * archivo2 = (char *) malloc (sizeof (char));
+	char * archivo_salida = (char *) malloc (sizeof (char));
 	system ("cls");
 	if (argc < 4)
 	{
@@ -23,123 +37,118 @@ int main (int argc, char const *argv[])
 	{
 		archivo1 = (char *) argv [1];
 		archivo2 = (char *) argv [2];
-		salida = (char *) argv [3];
+		archivo_salida = (char *) argv [3];
 	}
 
 	//Abrimos los archivos en modo binario
-	archivo_1 = abre_archivo (archivo1, archivo2, salida, 1);
-	archivo_2 = abre_archivo (archivo1, archivo2, salida, 2);
-	archivoSalida = abre_archivo (archivo1, archivo2, salida, 3);
+	archivo_1 = fopen (archivo1, "rb");
+	if (archivo_1 == NULL)
+	{
+		printf ("Error al abrir el archivo: '%s'", archivo1);
+		exit (0);
+	}
+	archivo_2 = fopen (archivo2, "rb");
+	if (archivo_2 == NULL)
+	{
+		printf ("Error al abrir el archivo: '%s'", archivo2);
+		exit (0);
+	}
+	salida = fopen (archivo_salida, "wb");
+	if (salida == NULL)
+	{
+		printf ("Error al crear el archivo: '%s'", archivo_salida);
+		exit (0);
+	}
 
-	//Leemos e imprimimos la cabecera del archivo wav
-	leer_cabecera (archivo_1, archivo_2, &cab_1, &cab_2);
+	//Leer las cabeceras de ambos archivos de entrada
+	fread (&cab_1, 44, 1, archivo_1);
+	fread (&cab_2, 44, 1, archivo_2);
+
+	//Imprimir los valores de la cabecera
 	imprimir_cabecera (&cab_1, 1);
 	imprimir_cabecera (&cab_2, 2);
 
-	if ((cab_1.ChunkSize) >= (cab_2.ChunkSize))
-		copiar_cabecera (archivo_1, archivoSalida, &cab_1);
+	numero_muestras_1 = (cab_1.SubChunk2Size / cab_1.BlockAlign);
+	numero_muestras_2 = (cab_2.SubChunk2Size / cab_2.BlockAlign);
+
+	//Copiar cabecera del archivo más grande en el de salida
+	if (numero_muestras_1 >= numero_muestras_2)
+		fwrite (&cab_1, 44, 1, salida);
 	else
-		copiar_cabecera (archivo_2, archivoSalida, &cab_2);
+		fwrite (&cab_2, 44, 1, salida);
 
-	fseek (archivoSalida, 44, SEEK_SET);
-	fseek (archivo_1, 44, SEEK_SET);
-	fseek (archivo_2, 44, SEEK_SET);
-
-	if ((cab_1.NumChannels == 1) && (cab_2.NumChannels == 1))					//Si son 2 archivos mono
+	if ((cab_1.NumChannels == 1) && (cab_2.NumChannels == 1))
 	{
-		if ((cab_1.SubChunk2Size) >= (cab_2.SubChunk2Size))
+		if (numero_muestras_1 >= numero_muestras_2)
 		{
-			for (i = 0; i < (cab_1.SubChunk2Size / 2); i ++)
-			{
-				fread (&real1, sizeof (short), 1, archivo_1);
-				real1_1 = (real1 / max);
-				if (i >= (cab_2.SubChunk2Size / 2))
-					real2 = 0;
-				else
-					fread (&real2, sizeof (short), 1, archivo_2);
-				real2_1 = (real2 / max);
-				real1 = (real1_1 * real2_1 * max / 2);
-				fwrite (&real1, sizeof (short), 1, archivoSalida);
-			}
+			numero_muestras = numero_muestras_1;
+			aux = numero_muestras_2;
+			grande = archivo_1;
+			chico = archivo_2;
 		}else
 		{
-			for (i = 0; i < (cab_2.SubChunk2Size / 2); i ++)
-			{
-				fread (&real1, sizeof (short), 1, archivo_2);
-				real1_1 = (real1 / max);
-				if (i >= (cab_1.SubChunk2Size / 2))
-					real2 = 0;
-				else
-					fread (&real2, sizeof (short), 1, archivo_1);
-				real2_1 = (real2 / max);
-				real1 = (real1_1 * real2_1 * max / 2);
-				fwrite (&real1, sizeof (short), 1, archivoSalida);
-			}
+			numero_muestras = numero_muestras_2;
+			aux = numero_muestras_1;
+			grande = archivo_2;
+			chico = archivo_1;
+		}
+		for (i = 0; i < numero_muestras; i ++)
+		{
+			fread (&real1, sizeof (short), 1, grande);
+			real1_1 = (real1 / max);
+			if (i >= aux)
+				real2 = 0;
+			else
+				fread (&real2, sizeof (short), 1, chico);
+			real2_1 = (real2 / max);
+			real1 = (real1_1 * real2_1 * max / 2);
+			fwrite (&real1, sizeof (short), 1, salida);
 		}
 	}else
 	{
-		if ((cab_1.SubChunk2Size) >= (cab_2.SubChunk2Size))
+		if (numero_muestras_1 >= numero_muestras_2)
 		{
-			for (i = 0; i < (cab_1.SubChunk2Size / 4); i ++)
-			{
-				//Guardamos la parte real del número complejo
-				fread (&real1, sizeof (short), 1, archivo_1);
-				real1_1 = (real1 / max);
-				if (i >= (cab_2.SubChunk2Size / 4))
-				{
-					real2 = 0;
-					imaginario2 = 0;
-				}else
-				{
-					fread (&real2, sizeof (short), 1, archivo_2);
-					fread (&imaginario2, sizeof (short), 1, archivo_2);
-				}
-				real2_1 = (real2 / max);
-
-				//Guardamos la parte imaginaria del número complejo
-				fread (&imaginario1, sizeof (short), 1, archivo_1);
-				imaginario1_1 = (imaginario1 / max);
-				imaginario2_1 = (imaginario2 / max);
-
-				real1 = (((real1_1 * real2_1) - (imaginario1_1 * imaginario2_1)) * max / 2);
-				imaginario1 = (((real1_1 * imaginario2_1) + (imaginario1_1 * real2_1)) * max / 2);
-				fwrite (&real1, sizeof (short), 1, archivoSalida);
-				fwrite (&imaginario1, sizeof (short), 1, archivoSalida);
-			}
+			numero_muestras = numero_muestras_1;
+			aux = numero_muestras_2;
+			grande = archivo_1;
+			chico = archivo_2;
 		}else
 		{
-			for (i = 0; i < (cab_2.SubChunk2Size / 4); i ++)
+			numero_muestras = numero_muestras_2;
+			aux = numero_muestras_1;
+			grande = archivo_2;
+			chico = archivo_1;
+		}
+		for (i = 0; i < numero_muestras; i ++)
+		{
+			//Parte real del número complejo
+			fread (&real1, sizeof (short), 1, grande);
+			real1_1 = (real1 / max);
+			if (i >= aux)
 			{
-				//Guardamos la parte real del número complejo
-				fread (&real1, sizeof (short), 1, archivo_2);
-				real1_1 = (real1 / max);
-				if (i >= (cab_1.SubChunk2Size / 4))
-				{
-					real2 = 0;
-					imaginario2 = 0;
-				}else
-				{
-					fread (&real2, sizeof (short), 1, archivo_1);
-					fread (&imaginario2, sizeof (short), 1, archivo_1);
-				}
-				real2_1 = (real2 / max);
-
-				//Guardamos la parte imaginaria del número complejo
-				fread (&imaginario1, sizeof (short), 1, archivo_2);
-				imaginario1_1 = (imaginario1 / max);
-				imaginario2_1 = (imaginario2 / max);
-
-				real1 = (((real1_1 * real2_1) - (imaginario1_1 * imaginario2_1)) * max / 2);
-				imaginario1 = (((real1_1 * imaginario2_1) + (imaginario1_1 * real2_1)) * max / 2);
-				fwrite (&real1, sizeof (short), 1, archivoSalida);
-				fwrite (&imaginario1, sizeof (short), 1, archivoSalida);
+				real2 = 0;
+				imaginario2 = 0;
+			}else
+			{
+				fread (&real2, sizeof (short), 1, chico);
+				fread (&imaginario2, sizeof (short), 1, chico);
 			}
+			real2_1 = (real2 / max);
+
+			//Parte imaginaria del número complejo
+			fread (&imaginario1, sizeof (short), 1, grande);
+			imaginario1_1 = (imaginario1 / max);
+			imaginario2_1 = (imaginario2 / max);
+
+			real1 = (((real1_1 * real2_1) - (imaginario1_1 * imaginario2_1)) * max / 2);
+			imaginario1 = (((real1_1 * imaginario2_1) + (imaginario1_1 * real2_1)) * max / 2);
+			fwrite (&real1, sizeof (short), 1, salida);
+			fwrite (&imaginario1, sizeof (short), 1, salida);
 		}
 	}
 	printf ("\n\n");
 	fclose (archivo_1);
 	fclose (archivo_2);
-	fclose (archivoSalida);
-	printf ("Archivos '%s' y '%s' multiplicados correctamente en '%s'.\n\n", archivo1, archivo2, salida);
+	fclose (salida);
 	return 0;
 }
