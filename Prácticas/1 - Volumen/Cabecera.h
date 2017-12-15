0f3d7502a2df7e3ebbad78f72881fb1aedbd5968
Joel Romero <joelrg1288@gmail.com>
2017-12-14 21:26:20 -0600

    Práctica completa :D

--- Prácticas/1 - Volumen/Cabecera.h
+++ Prácticas/1 - Volumen/Cabecera.h
@@ -1,22 +1,22 @@
 typedef struct CABECERA
 {
-	char ChunkID[4];					//Contiene las 'RIFF'
-	int ChunkSize;						//Contiene el tamaño total sin contar este y el segmento anterior (8 bytes)
+	char ChunkID[4];					//Contiene 'RIFF'
+	int ChunkSize;						//Contiene el tamaño total del archivo - 8 bytes
 	char Format[4];						//Contiene 'WAVE'
 
 	//Aqui comienza el primer subchunk 'fmt'
 	char SubChunk1ID[4];				//Contiene 'fmt'
-	int SubChunk1Size;					//Contiene el tamaño del resto de el primer subchunk
-	short AudioFormat;					//Formato de audio, es es distinto de 1, es forma de compresión
-	short NumChannels;					//Numero de canales, mono = 1, estereo = 2, etc.
+	int SubChunk1Size;					//Contiene el tamaño del primer subchunk
+	short AudioFormat;					//Formato de audio
+	short NumChannels;					//Numero de canales
 	int SampleRate;						//Frecuencia de muestreo
 	int ByteRate;						//(SampleRate * Numero canales * Bits per Sample) / 8
-	short BlockAlign;					//Bytes por muestra: (Numero canales * Bits per Sample) / 8
+	short BlockAlign;					//Bytes por muestra
 	short BitsPerSample;				//8 bits, 16 bits, etc.
 	
 	//Aqui comienza el segundo subchunk 'data'
 	char SubChunk2ID[4];				//Contiene 'data'
-	int SubChunk2Size;					//Numero de bytes en los datos, es decir, bytes despues de este segmento
+	int SubChunk2Size;					//Numero de bytes en los datos
 }cabecera;
 
 void imprimir_cabecera (cabecera * cab);
