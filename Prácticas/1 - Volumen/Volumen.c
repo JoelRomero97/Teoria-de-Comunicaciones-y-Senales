1c35b3354dad7572b9928a08f3efd8128e10ba05
Joel Romero <joelrg1288@gmail.com>
2017-12-16 12:58:58 -0600

    Práctica completa :D

--- Prácticas/1 - Volumen/Volumen.c
+++ Prácticas/1 - Volumen/Volumen.c
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
 	if (argc < 3)
 	{
 		printf("Error, faltan argumentos.\n");
-		printf ("Ejemplo: '%s Archivo.wav Salida.wav'\n\n", argv [0]);
+		printf ("Ejemplo: '%s Entrada.wav Salida.wav'\n\n", argv [0]);
 		exit (0);
 	}else
 	{
