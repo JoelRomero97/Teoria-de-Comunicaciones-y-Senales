7ded936431b2ca753333f2c929dae6af7bcee3ba
Joel Romero <joelrg1288@gmail.com>
2017-12-18 01:18:24 -0600

    Se modifica el intervalo de lectura de sonido

--- Proyecto/Sonido.c
+++ Proyecto/Sonido.c
@@ -7,20 +7,20 @@
 int main(int argc, char const *argv[])
 {
 	system ("clear");
-	int i, flag;
-	int tam;
+	int flag, tam, i;
 	unsigned int segundos;
 	snd_pcm_t * manejador;
 	snd_pcm_uframes_t muestras = 32;
-
 	manejador = establecer_parametros (manejador, &tam, &segundos);
-
-	char * datos = (char *) malloc (sizeof (char) * tam);
-
+	short * datos = (short *) malloc (sizeof (short) * tam);
+	int ciclo = 1;
 	while (segundos > 0)
 	{
 		segundos = (segundos - 1);
 		flag = snd_pcm_readi (manejador, datos, muestras);
+		//for (i = 0; i < (sizeof (datos)); i ++)
+		//	printf ("Muestra del ciclo %d: %d\n", ciclo, datos [i]);
+		//ciclo ++;
 		if (flag < 0)
 		{
 			printf ("\n\nNo fue posible recibir audio: %s\n\n", snd_strerror (flag));
@@ -31,6 +31,8 @@ int main(int argc, char const *argv[])
 			exit (1);
 		}
 	}
+	//generar_archivo_wav (datos);
+	//Ya que se obtiene el sonido, cerramos el manejador
 	snd_pcm_close (manejador);
 	return 0;
 }
