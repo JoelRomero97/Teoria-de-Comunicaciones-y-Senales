acad0cfc35d1662a717262cf64f07e495696c7ae
Joel Romero <joelrg1288@gmail.com>
2017-12-18 01:22:08 -0600

    Version 1 (estable) del proyecto

--- Proyecto/Sonido.c
+++ Proyecto/Sonido.c
@@ -2,47 +2,43 @@
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
 #include <unistd.h>
-#include "Funciones/Funciones.c"
+#include <string.h>
+#include "Funciones.h"
 
 int main(int argc, char const *argv[])
 {
 	system ("clear");
-	int i, j, flag, muestras = 128;
-	int tam;
-	unsigned int canales = 2, latencia = 500000;
-	int frecuencia = 44100;
+	int i, flag, tam;
+	char tecla, aux = 'z';
+	unsigned int segundos;
 	snd_pcm_t * manejador;
-	snd_pcm_format_t formato = SND_PCM_FORMAT_S16_LE;			//Little Endian
-
-	manejador = establecer_parametros (manejador);
-
-	flag = snd_pcm_prepare (manejador);
-	if (flag < 0)
+	snd_pcm_uframes_t muestras = SAMPLES;
+	manejador = establecer_parametros (manejador, &tam, &segundos);
+	short * datos = (short *) malloc (sizeof (short) * tam);
+	snd_pcm_prepare (manejador);
+	while (segundos > 0)
 	{
-		printf ("\n\nNo fue posible preparar la interfaz de audio para usarse: %s\n", snd_strerror (flag));
-		exit (1);
-	}else
-		printf ("Interfaz de audio lista.\n");
-
-	tam = (((muestras * snd_pcm_format_width (formato)) / 8) * 2);
-	short * datos = (short *) malloc (tam);
-
-	for (i = 0; i < 10; i ++)
-	{
+		//segundos = (segundos - 1);
 		flag = snd_pcm_readi (manejador, datos, muestras);
-		if (flag != muestras)
+		if (flag < 0)
 		{
-			printf ("Error al recibir el audio: %s", snd_strerror (flag));
-			exit (1);
+			printf ("\n\nNo fue posible recibir audio: %s\n\n", snd_strerror (flag));
+			snd_pcm_prepare (manejador);
+		}else if (flag == -EPIPE)
+		{
+			printf ("\n\nSe recibieron mas muestras de las esperadas: %s\n\n", snd_strerror (flag));
+			snd_pcm_prepare (manejador);
 		}else
 		{
-			for (j = 0; j < tam; j ++)
-				if (datos [j] != 0)
-					printf ("Muestra %d del ciclo %d: %d\n", (j + 1), (i + 1), datos [j]);
-			printf ("\n\n");
+			tecla = tecla_presionada (datos);
+			if (aux == tecla)
+				printf ("Tecla presionada: %c\n\n", tecla);
+			else
+				aux = tecla;
 		}
 	}
-
+	//Ya que se obtiene el sonido, cerramos el manejador
 	snd_pcm_close (manejador);
+	free (datos);
 	return 0;
 }
