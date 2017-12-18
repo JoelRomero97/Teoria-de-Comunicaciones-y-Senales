9786cdb6f9061ec7438056e5dd6321448b26c573
Joel Romero <joelrg1288@gmail.com>
2017-12-18 17:40:09 -0600

    Modificación del script para ejecutar proyecto

--- Proyecto/Proyecto.sh
+++ Proyecto/Proyecto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -f DTMF ]; then
 	rm DTMF
 fi
@@ -10,4 +9,4 @@ fi
 
 gcc Funciones.c -o Funciones.o -c -lm
 gcc DTMF.c -o DTMF Funciones.o -lm -lasound
-./DTMF
+./DTMF 3905 127.0.0.1
